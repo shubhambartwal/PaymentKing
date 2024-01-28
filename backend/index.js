35c6935d2c4948e6bb178a467732dbc081f89976
shubhambartwal <shubhambartwal58@gmail.com>
2024-01-28 20:49:22 +0530

    adding jwt token,added listener to app

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
+const cors= require("cors");
 const app = express();
 app.use(cors());
+app.use(express.json())
 app.use("/api/v1", rootRouter);
+app.listen(3000,function(){
+    console.log("port 3000!!!")
+})
